356c2cbb25e38d8cb4593ef98be3a12f51940896
UseR <highermath80@gmail.com>
2022-02-14 13:30:43 +0600

    Initial commit

---
+++ civangolu/resources/views/components/header.blade.php
@@ -0,0 +1,23 @@
+<div class="fixed w-full py-4 px-4 flex justify-between items-center z-30">
+    <div class="min-w-max">
+        <a href=""><img width="100" src="/img/logo.jpeg" alt=""></a>
+    </div>
+
+    <div class="w-full">
+        <ul class="flex justify-center">
+            <li><a class="inline-block px-4 py-2 text-white" href="">Land</a></li>
+            <li><a class="inline-block px-4 py-2 text-white" href="">Villa</a></li>
+            <li><a class="inline-block px-4 py-2 text-white" href="">Apartment</a></li>
+            <li><a class="inline-block px-4 py-2 text-white" href="">About Us</a></li>
+            <li><a class="inline-block px-4 py-2 text-white" href="">Contact Us</a></li>
+        </ul>
+    </div>
+
+
+    <div class="min-w-max flex">
+        <a class="pr-4" href=""><img src="https://flagcdn.com/16x12/tr.png"></a>
+        <a href=""><img src="https://flagcdn.com/16x12/us.png"></a>
+    </div>
+
+
+</div>
---
+++ civangolu/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-guest-layout>
+    <div class="relative z-10 pt-48 pb-52 bg-cover bg-center" style="background-image: url(/img/hero-bg.jpg)">
+        <div class="absolute h-full w-full bg-black opacity-30 top-0 left-0 z-10"></div>
+        <div class="container relative z-20 text-white text-center text-2xl">
+            <h2 class="font-bold text-5xl mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing.<br/> Lorem ipsum dolor sit amet.</h2>
+            <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat fugit illum officia quisquam recusandae sapiente voluptatum.</p>
+        </div>
+    </div>
+
+</x-guest-layout>
